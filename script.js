6e5317b06667fc9b222d1744e2df5c84137e7ed7
anshdhiman099 <anshdhiman099@gmail.com>
2026-06-16 23:01:08 +0530

    Add files via upload
    Initial NGO Website Project

---
+++ script.js
@@ -0,0 +1,71 @@
+// ===============================
+// Dark Mode
+// ===============================
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+if (darkModeBtn) {
+
+    darkModeBtn.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark-mode");
+
+    });
+
+}
+
+
+// ===============================
+// Volunteer Form
+// ===============================
+
+const volunteerForm = document.getElementById("volunteerForm");
+
+if (volunteerForm) {
+
+    volunteerForm.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        const successMessage =
+            document.getElementById("successMessage");
+
+        successMessage.innerHTML =
+            "✅ Thank You for Registering! We will contact you soon.";
+
+        successMessage.style.color = "green";
+        successMessage.style.marginTop = "20px";
+        successMessage.style.fontSize = "20px";
+
+        volunteerForm.reset();
+
+    });
+
+}
+// ===============================
+// Contact Form
+// ===============================
+
+const contactForm = document.getElementById("contactForm");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function (event) {
+
+        event.preventDefault();
+
+        const contactSuccess =
+            document.getElementById("contactSuccess");
+
+        contactSuccess.innerHTML =
+            "✅ Your message has been sent successfully!";
+
+        contactSuccess.style.color = "green";
+        contactSuccess.style.fontSize = "20px";
+        contactSuccess.style.marginTop = "20px";
+
+        contactForm.reset();
+
+    });
+
+}
