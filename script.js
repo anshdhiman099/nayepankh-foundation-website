c972e4bb6a28f75ab9c763f1c7b3c95a4b85cf75
anshdhiman099 <anshdhiman099@gmail.com>
2026-06-16 23:56:46 +0530

    Refactor script.js to add menu toggle functionality

--- script.js
+++ script.js
@@ -69,3 +69,9 @@ if (contactForm) {
     });
 
 }
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
